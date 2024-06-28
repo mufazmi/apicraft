04a81c0fb8374999bbd0a471f519cb26380f812e
mufazmi <info.mufazmi@gmail.com>
2024-06-28 08:52:05 +0530

    Bug Fixes

--- templates/services/service.ts
+++ templates/services/service.ts
@@ -1,17 +1,17 @@
 import { FilterQuery } from "mongoose";
-import { Base, IBase } from "../models/base-kebab-model";
+import BaseModel, { IBase } from "../models/base-kebab-model";
 
 class BaseService {
 
-    create = async (data: IBase) => await Base.create(data);
+    create = async (data: IBase) => await BaseModel.create(data);
 
-    findOne = async (filter: any) => await Base.findOne(filter);
+    findOne = async (filter: any) => await BaseModel.findOne(filter);
 
-    findAll = async (filter: any) => await Base.find(filter);
+    findAll = async (filter: any) => await BaseModel.find(filter);
 
-    update = async (filter: any, data: any) => await Base.findOneAndUpdate(filter, data, { new: true });
+    update = async (filter: any, data: any) => await BaseModel.findOneAndUpdate(filter, data, { new: true });
 
-    destroy = async (filter: any) => await Base.deleteMany(filter);
+    destroy = async (filter: any) => await BaseModel.deleteMany(filter);
 }
 
 export default new BaseService;
