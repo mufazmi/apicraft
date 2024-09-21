63e3afaf2757603d29aa9cea073ecf4f34f764ba
mufazmi <info.mufazmi@gmail.com>
2024-09-21 07:41:06 +0530

    improvement

--- templates/models/model.ts
+++ templates/models/model.ts
@@ -1,22 +1,36 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
+import CompanyModel from "./company-model";
 
-export interface IBase extends Document {
+export interface IBase {
   name: string;
+  company: Types.ObjectId;
 }
 
-const baseSchema = new Schema<IBase>({
-  name: {
-    type: String,
-    required: true
+export interface IBaseDocument extends Document, IBase {
+  
+}
+
+const baseSchema = new Schema<IBase>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: CompanyModel,
+      required: true,
+    },
   },
-},
-{
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  },
-  collection: 'collectionName'
-});
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+    collection: 'collectionName',
+  }
+);
 
 const BaseModel = model<IBase>('Base', baseSchema, 'collectionName');
 
